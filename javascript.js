e6ceed0b58c1b7362ebe733cf0ffc7958d12d259
Arya Poudel <arya.poudel20@gmail.com>
2020-11-07 16:32:57 +0545

    Add . and fix =

--- javascript.js
+++ javascript.js
@@ -1,58 +1,71 @@
+let clear = false; begin = true;
+let number1, number2, operate_with, nextoperator, calculated;
+
+const result = document.getElementById('result');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const ac = document.getElementById('ac');
+
+
 function operate(a,b,sign) {
 	if (sign == '+') { return parseInt(a) + parseInt(b) ;}
 	if (sign == '-') { return a - b ;}
 	if (sign == '*') { return a * b ;}
 	if (sign == '/') { return a / b ;}
-	if (sign == '=') { return a; } //todo
+	if (sign == '=') { return a;} 
 }
 
-let clear = false; begin = true;
-let number1, number2, operate_with, nextoperator, calculated;
 
-const result = document.getElementById('result');
-
-const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => number.addEventListener('click', () =>
-	{
-		if (clear) { 
-			result. textContent = '';
-		}
-		result.textContent = result.textContent + number.textContent;
-		clear = false;
-	}));
+{
+	if (clear) { 
+		result.textContent = '';
+	}
 
-const operators = document.querySelectorAll('.operator');
+	if (number.textContent=='.' && result.textContent.includes('.')) {
+		return;
+	} 
+	result.textContent = result.textContent + number.textContent;
+	clear = false;
+}));
+
+
 operators.forEach(operator => operator.addEventListener('click' , () =>
-	{
-		clear = true;
-		operate_with = nextoperator;
-		nextoperator = operator.textContent;
-		if ( calculated == undefined) {
-			if (begin) {
-				number1 = result.textContent;
-				begin = false;
-			} 
-			else {
-				number2 = result.textContent;
-				calculated = operate (number1, number2, operate_with);
-				result.textContent = calculated;
-			}
+{
+	clear = true;
+	operate_with = nextoperator;
+	nextoperator = operator.textContent;
+	if ( calculated == undefined) {
+		if (begin) {
+			number1 = result.textContent;
+			begin = false;
+		} 
+		else {
+			number2 = result.textContent;
+			calculated = operate (number1, number2, operate_with);
+			result.textContent = calculated;
+		}
+	} 
+	else {
+		if (operate_with == '=' && result.textContent != calculated) {
+			number1 = result.textContent;
 		} 
 		else {
 			number1 = calculated;
-			number2 = result.textContent;
-			calculated = operate (number1 , number2, operate_with);
-			result.textContent = calculated;
-		}		
-	}));
+		}
+		number2 = result.textContent;
+		console.log(number2);
+		calculated = operate (number1 , number2, operate_with);
+		result.textContent = calculated;
+	}		
+}));
 
-const ac = document.getElementById('ac');
+
 ac.addEventListener('click' , () =>
-	{
-		result.textContent = '';
-		clear = false;
-		begin = true;
-		calculated = undefined;
+{
+	result.textContent = '';
+	clear = false;
+	begin = true;
+	calculated = undefined;
 
-	});
-
+});
